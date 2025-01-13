ef715276333e4ee1971a5422acac05b04745de51
arunvvtsolutions <arun.marimuthu@vvtsolutions.in>
2025-01-13 15:04:33 +0530

    edit 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,29 +34,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <Script
-          id="googleTag"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NSKKPP2');
-          `
-          }}
-          strategy="worker"
-        />
+       
       </head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NSKKPP2"
-            height="0"
-            width="0"
-            style={GoogleTagStyles}
-          ></iframe>
-        </noscript>
+       
         {/* <ClientLayout>{children}</ClientLayout> */}
         <h1>Hello</h1>
         <Link target="blank" href="https://www.w3schools.com/">Click here</Link>
